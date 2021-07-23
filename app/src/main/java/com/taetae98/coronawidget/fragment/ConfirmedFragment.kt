d0604e9eb4a9cdb1538fe3e78c901eb30ca3883f
KangTaeJong98 <rkdxowhd98@naver.com>
2021-07-23 13:20:58 +0900

    CORONA-6 PhaseFragment 기능구현

--- app/src/main/java/com/taetae98/coronawidget/fragment/ConfirmedFragment.kt
+++ app/src/main/java/com/taetae98/coronawidget/fragment/ConfirmedFragment.kt
@@ -1,12 +1,5 @@
 package com.taetae98.coronawidget.fragment
 
-import android.annotation.SuppressLint
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.webkit.WebChromeClient
-import android.webkit.WebViewClient
 import androidx.fragment.app.activityViewModels
 import com.taetae98.coronawidget.R
 import com.taetae98.coronawidget.databinding.BindingFragment
@@ -18,24 +11,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class ConfirmedFragment : BindingFragment<FragmentConfirmedBinding>(R.layout.fragment_confirmed) {
     private val viewModel by activityViewModels<ConfirmedViewModel>()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        super.onCreateView(inflater, container, savedInstanceState)
-        onCreateWebView()
-
-        return binding.root
-    }
-
     override fun onCreateViewDataBinding() {
         super.onCreateViewDataBinding()
         binding.viewModel = viewModel
-    }
-
-    @SuppressLint("SetJavaScriptEnabled")
-    private fun onCreateWebView() {
-        with(binding.webView) {
-            settings.javaScriptEnabled = true
-            webChromeClient = WebChromeClient()
-            webViewClient = WebViewClient()
-        }
     }
 }
---
+++ app/src/main/java/com/taetae98/coronawidget/view/JavaScriptWebView.kt
@@ -0,0 +1,22 @@
+package com.taetae98.coronawidget.view
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+@SuppressLint("SetJavaScriptEnabled")
+class JavaScriptWebView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) : WebView(context, attrs, defStyleAttr, defStyleRes) {
+    init {
+        settings.javaScriptEnabled = true
+        webViewClient = WebViewClient()
+        webChromeClient = WebChromeClient()
+    }
+}
