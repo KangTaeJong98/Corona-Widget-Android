c7865f89c5f271e8bc2d6bf84edb75297cf17a4b
KangTaeJong98 <rkdxowhd98@naver.com>
2021-07-25 19:42:52 +0900

    CORONA-11 KoreaCoronaWidget 구현?

--- app/src/main/java/com/taetae98/coronawidget/base/BaseFragment.kt
+++ app/src/main/java/com/taetae98/coronawidget/base/BaseFragment.kt
@@ -1,5 +1,6 @@
 package com.taetae98.coronawidget.base
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
@@ -7,6 +8,10 @@ import androidx.fragment.app.Fragment
 abstract class BaseFragment : Fragment() {
     protected fun setSupportActionBar(toolbar: Toolbar) {
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
+    }
+
+    protected fun setResult(resultCode: Int, intent: Intent? = null) {
+        requireActivity().setResult(resultCode, null)
     }
 
     protected fun finish() {
